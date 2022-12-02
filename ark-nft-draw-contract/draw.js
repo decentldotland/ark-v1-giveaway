8772d8da1f2e2bb6e88b1836e92429a6f20527cb
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-12-02 10:41:22 +0100

    feat: rewrite contract in molecule.sh

--- ark-nft-draw-contract/draw.js
+++ ark-nft-draw-contract/draw.js
@@ -1,72 +1,36 @@
 export async function handle(state, action) {
   const input = action.input;
 
-  const contractAdmin = state.contractAdmin;
-  const participants = state.participants;
-  const signatures = state.signatures;
-  const winners = state.winners;
-  const maxIndexLimit = participants?.length - 1;
-
-  const ERROR_SOMETHING_WENT_WRONG = `ERROR_GENETRATING_RANDOM_INTEGER_FROM_RANDOM_DOT_ORG`;
-  const ERROR_INVALID_CALLER = `ERROR_ONLY_CONTRACT_ADMIN_CAN_CALL_THIS_FUNCTION`;
-  const ERROR_SIGNATURE_ALREADY_USED = `ERROR_ADMIN_SIGNATURE_REUSAGE`;
-  const ERROR_INVALID_CALLER_SIGNATURE = `ERROR_INVALID_ADMIN_SIGNATURE`;
-
   if (input.function === "draw") {
-    const jwk_n = input.jwk_n;
-    const sig = input.sig;
+    ContractAssert(
+      state.participants.length,
+      "cannot proceed with an empty participants array"
+    );
+    ContractAssert(!state.isSealed,"draw closed - done");
 
-    await _verifyArSignature(jwk_n, sig);
-    const drawId = SmartWeave.transaction.id;
-    const luckyNumberIndex = await _generateRandomNumber();
-
-    winners.push({
-      address: participants[luckyNumberIndex],
-      luckyNumber: luckyNumberIndex,
-      draw_id: drawId,
-      signature: sig,
+    const max = state.participants.length - 1;
+    const drawResult = await _moleculeRandInt(max);
+    state.winners.push({
+      winner: state.participants[drawResult],
+      randInt: drawResult,
     });
+
+    state.isSealed = true;
 
     return { state };
   }
 
-  async function _generateRandomNumber() {
+  async function _moleculeRandInt(max) {
     try {
+      // min param is assigned to 0 because the draw particiants is an non-empty array
       const req = await EXM.deterministicFetch(
-        `https://www.random.org/integers/?num=1&min=0&max=${maxIndexLimit}&col=1&base=10&format=plain`
+        `${state.rand_molecule_endpoint}/generate/0/${max}`
       );
-      const res = Number(req?.asText());
-      return res;
+      const index = req.asJSON()?.result;
+      ContractAssert(typeof index === "number", "molecule rand error");
+      return index;
     } catch (error) {
-      throw new ContractError(ERROR_SOMETHING_WENT_WRONG);
-    }
-  }
-
-  async function _verifyArSignature(owner, signature) {
-    try {
-      ContractAssert(owner === contractAdmin, ERROR_INVALID_CALLER);
-      ContractAssert(
-        !state.signatures.includes(signature),
-        ERROR_SIGNATURE_ALREADY_USED
-      );
-
-      const encodedMessage = new TextEncoder().encode(
-        `ARK NFT LUCKY DRAW: ${owner}`
-      );
-      const typedArraySig = Uint8Array.from(atob(signature), (c) =>
-        c.charCodeAt(0)
-      );
-      const isValid = await SmartWeave.arweave.crypto.verify(
-        owner,
-        encodedMessage,
-        typedArraySig
-      );
-
-      ContractAssert(isValid, ERROR_INVALID_CALLER_SIGNATURE);
-
-      state.signatures.push(signature);
-    } catch (error) {
-      throw new ContractError(ERROR_INVALID_CALLER_SIGNATURE);
+      throw new ContractError("error molecule rand connection");
     }
   }
 }
